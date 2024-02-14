da23703c7cb584e65d5ea1d75f5079071c5292e8
Ruben <112164477+Ruuebn@users.noreply.github.com>
2024-02-14 20:20:27 +0100

    Added find_factors function to retrieve factors

--- pollardrho.cpp
+++ pollardrho.cpp
@@ -10,7 +10,6 @@ void pollardrho::f(mpz_t& x, mpz_t n) {
     mpz_mul(x, x, x);
     mpz_add_ui(x, x, m_c);
     mpz_mod(x, x, n);
-    m_c += 1;
 }
 
 unsigned long int pollardrho::pollard_rho_algorithm(mpz_t n, mpz_t x_1) {
@@ -35,4 +34,23 @@ unsigned long int pollardrho::pollard_rho_algorithm(mpz_t n, mpz_t x_1) {
 
     if(mpz_cmp(p, n) == 0) return res;
     return res;
+}
+
+// Will do the amount of reps to find factors (unique but might share common factors)
+std::vector<unsigned long int> pollardrho::find_factors(mpz_t n, mpz_t x_1) {
+    std::vector<unsigned long int> factors;
+
+    for(size_t i = 0; i < m_reps; ++i) {
+        for(size_t j = 0; j < m_reps; ++j) {
+            unsigned long int res = pollard_rho_algorithm(n, x_1);
+            //gmp_printf("FACTOR FOUND: %lu\n", res);
+            auto it = std::find(factors.begin(), factors.end(), res);
+            if(it == factors.end()) factors.push_back(res);
+
+            mpz_add_ui(x_1, x_1, 1);
+        }
+        m_c += 1;
+    }
+
+    return factors;
 }
--- pollardrho.hpp
+++ pollardrho.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
 #include <gmpxx.h>
-#include <list>
+#include <vector>
 
 class pollardrho {
+protected:
+    unsigned long int pollard_rho_algorithm(mpz_t n, mpz_t x_1);
 private:
     // Amount of repetitions, scales complexity quadratically
+    // because both c and x_1 is incremented by themselves
     unsigned int m_reps;
     // Constant c to use within f
     unsigned int m_c;
     // External function f to use
     void f(mpz_t& x, mpz_t n);
-public:
     // The infamous algorithm
-    unsigned long int pollard_rho_algorithm(mpz_t n, mpz_t x_1);
-
+public:
+    std::vector<unsigned long int> find_factors(mpz_t n, mpz_t x_1);
     pollardrho(unsigned int c, unsigned int reps);
 };

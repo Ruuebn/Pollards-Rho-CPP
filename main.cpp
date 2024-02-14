2cf77525f0f4182b025f6423a6891986eb7376ef
Ruben <112164477+Ruuebn@users.noreply.github.com>
2024-02-14 20:21:36 +0100

    Reworked to fit in with new function in pollardrho.cpp, added preprocessor flags to allow testing

--- main.cpp
+++ main.cpp
@@ -2,22 +2,57 @@
 
 #include "pollardrho.hpp"
 
+#ifdef RUN_TESTS
+
+#include "testpollardrho.hpp"
+
+#endif
+
+// Arguments: <number> <reps> where <number> is the number to be factored
+// and <reps> is the amount of times to try and find factors
+// if you want test cases to be run, simply add the -DRUN_TESTS
+// flag when compiling
+
 int main(int argc, char** argv) {
 
-    pollardrho pr(5, 10);
+    if(argc < 3) {
+        std::cerr << "Not enough arguments. Usage: " << argv[0] << " <number> <reps>\n";
+        return 1;
+    }
+
+// Runs tests on initialization if compiler flag is set
+#ifdef RUN_TESTS
+    testPollardRho test = testPollardRho();
+#endif
+    
+    unsigned int REPS = std::stoi(argv[2]);
+
+    pollardrho pr(1, REPS);
     mpz_t num, x_1;
     // mpz_inits is NULL-terminated
     mpz_inits(num, x_1, NULL);
-    mpz_set_ui(num, 60);
+    mpz_set_str(num, argv[1], 10);
     mpz_set_ui(x_1, 1);
 
-    for(unsigned int i = 0; i < 100; ++i) {
-        gmp_printf("Factor for num: %lu\n", pr.pollard_rho_algorithm(num, x_1));
-    }
+    gmp_printf("NUM to be factored: %Zd\n", num);
+    gmp_printf("With amount of REPS: %lu\n", REPS);
+    
+    std::vector<unsigned long int> factors = pr.find_factors(num, x_1);
 
+    std::cout << "Factors size: " << factors.size() << std::endl;
+
+    std::cout << "Factors found: ";
+    for(size_t i = 0; i < factors.size(); ++i) {
+        if(i + 1 == factors.size()) {
+            std::cout << factors[i] << std::endl;
+            break;
+        }
+        std::cout << factors[i] << ", ";
+    }
     
     // mpz_clears is NULL-terminated
     mpz_clears(num, x_1, NULL);
+
 
     return 0;
 }

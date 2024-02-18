46145779363eb0b9d023963fbca99f5c7d18a797
Ruben <112164477+Ruuebn@users.noreply.github.com>
2024-02-18 18:01:02 +0100

    Added function to print test results

--- testpollardrho.cpp
+++ testpollardrho.cpp
@@ -1,6 +1,7 @@
 #include "testpollardrho.hpp"
 #include <iostream>
 
+// Initialises child with c = x_1 = 1
 testPollardRho::testPollardRho() :
     pollardrho(1, 1)
 {
@@ -22,24 +23,24 @@ void testPollardRho::run_test_cases() {
 
     // This test is passes by "failing"
     std::cout << "TEST CASE 1: ";
-    if(expected_result != factor) {
-        std::cout << "Passed" << std::endl;
-    } else {
-        std::cout << "Failed, expected result was: ";
-        std::cout << expected_result << ", but got ";
-        std::cout << factor << " instead\n";
-    }
+    print_test_results(expected_result != factor, expected_result, factor);
 
-    // This test passes
     mpz_set_ui(n, 1000);
     factor = pollard_rho_algorithm(n, x_1);
     expected_result = 8;
+
+    // This test passes
     std::cout << "TEST CASE 2: ";
-    if(expected_result == factor) {
+    print_test_results(expected_result == factor, expected_result, factor);
+}
+
+// Prints out result based on method of evaluating
+void testPollardRho::print_test_results(bool method, int test_val, int actual_val) {
+    if(method) {
         std::cout << "Passed" << std::endl;
     } else {
         std::cout << "Failed, expected result was: ";
-        std::cout << expected_result << ", but got ";
-        std::cout << factor << " instead\n";
+        std::cout << test_val << ", but got ";
+        std::cout << actual_val << " instead\n";
     }
 }
--- testpollardrho.hpp
+++ testpollardrho.hpp
@@ -7,6 +7,7 @@ private:
     mpz_t n;
     mpz_t x_1;
     void run_test_cases();
+    void print_test_results(bool method, int test_val, int actual_val);
 public:
     testPollardRho();
     ~testPollardRho();
